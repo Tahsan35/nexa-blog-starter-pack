40887a7f5b909cfd8dc890555efe41f65c49f1a4
Tahan Shakil <tahsanknmas14@gmail.com>
2025-06-29 21:12:46 +0600

    added blog pages

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,33 @@
+import BlogCard from "@/components/ui/BlogCard";
+import { Blog } from "@/types";
+
+export const metadata = {
+  title: "NexaBlog | Blogs",
+  description:
+    "Welcome to NexaBlog – where innovation meets imagination in the dynamic realm of technology, offering a thrilling journey through the latest trends and groundbreaking discoveries in the world of tech!",
+};
+
+const BlogsPage = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return (
+    <div className="w-[90%] mx-auto">
+      <h1 className="text-3xl text-center my-5 font-bold">
+        Explore All <span className="text-teal-600">Blog</span>
+      </h1>
+      <p className="text-center text-gray-400 w-2/5 mx-auto">
+        <i>
+          Dive into the fascinating world of quantum computing, where unlocking
+          unprecedented computational power.
+        </i>
+      </p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 my-5">
+        {blogs.map((blog: Blog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BlogsPage;
